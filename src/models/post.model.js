61bb8845614462321fe2a308d791d1c2f034d823
Raúl Filigrana Villalba <raufilvil@gmail.com>
2025-06-12 21:31:51 +0200

    Corrijo created_at en post.model.js.

--- src/models/post.model.js
+++ src/models/post.model.js
@@ -1,92 +1,102 @@
-const db = require('../config/db.config');
-const dayjs = require('dayjs');
+const db = require("../config/db.config");
+const dayjs = require("dayjs");
 
 const selectAll = async ({ groupedByAuthor = false, page = 0, limit = 0 }) => {
-    let [ result ] = await db.query(`
-        SELECT t1.title, t1.description, t1.createdAt, t1.category, 
+  let [result] = await db.query(
+    `
+        SELECT t1.title, t1.description, t1.created_at, t1.category, 
         t2.name, t2.email, t2.image 
         FROM post AS t1 
         INNER JOIN author AS t2 
         ON t1.author_id = t2.id
-        ${groupedByAuthor ? 'ORDER BY t2.name' : ''}
-        ${limit > 0 ? 'LIMIT ?' : ''}
-        ${(limit > 0 && page > 0) ? 'OFFSET ?' : ''}
-        `, [limit, limit * (page - 1)]
-    );
+        ${groupedByAuthor ? "ORDER BY t2.name" : ""}
+        ${limit > 0 ? "LIMIT ?" : ""}
+        ${limit > 0 && page > 0 ? "OFFSET ?" : ""}
+        `,
+    [limit, limit * (page - 1)]
+  );
 
-    if (result.length === 0) {
-        return { error: "No posts found." };
-    }
-    
-    if (groupedByAuthor) {
-        oldName = '';
-        authorNumber = -1;
-        resultGroupedByAuthor = [];
-        result.forEach((post) => {
-            const currentName = post.name;
-            if (currentName !== oldName) {
-                authorNumber++;
-                resultGroupedByAuthor.push({
-                    author: { 
-                        name: currentName,
-                        email: post.email,
-                        image: post.image
-                    },
-                    posts: []
-                });
-                oldName = currentName;
-            }
-            resultGroupedByAuthor[authorNumber].posts.push({
-                title: post.title,
-                description: post.description,
-                createdAt: post.createdAt,
-                category: post.category
-            });
-        })
-        result = resultGroupedByAuthor;
-    }
+  if (result.length === 0) {
+    return { error: "No posts found." };
+  }
 
-    return { 
-        page: (page > 0 && limit > 0) ? page : undefined, 
-        limit: (limit > 0) ? limit : undefined, 
-        posts: result
-    };
-}
+  if (groupedByAuthor) {
+    oldName = "";
+    authorNumber = -1;
+    resultGroupedByAuthor = [];
+    result.forEach((post) => {
+      const currentName = post.name;
+      if (currentName !== oldName) {
+        authorNumber++;
+        resultGroupedByAuthor.push({
+          author: {
+            name: currentName,
+            email: post.email,
+            image: post.image,
+          },
+          posts: [],
+        });
+        oldName = currentName;
+      }
+      resultGroupedByAuthor[authorNumber].posts.push({
+        title: post.title,
+        description: post.description,
+        created_at: post.created_at,
+        category: post.category,
+      });
+    });
+    result = resultGroupedByAuthor;
+  }
 
+  return {
+    page: page > 0 && limit > 0 ? page : undefined,
+    limit: limit > 0 ? limit : undefined,
+    posts: result,
+  };
+};
 
 const selectByAuthorId = async ({ author_id, page = 0, limit = 0 }) => {
-    const [ result ] = await db.query(`
-        SELECT title, description, createdAt, category 
+  const [result] = await db.query(
+    `
+        SELECT title, description, created_at, category 
         FROM post 
         WHERE author_id = ?
-        ${limit > 0 ? 'LIMIT ?' : ''}
-        ${(limit > 0 && page > 0) ? 'OFFSET ?' : ''}
-        `, [ author_id, limit, limit * (page - 1)]
-    );
-    
-    if (result.length === 0) {
-        return { error: "No posts found." };
-    }
+        ${limit > 0 ? "LIMIT ?" : ""}
+        ${limit > 0 && page > 0 ? "OFFSET ?" : ""}
+        `,
+    [author_id, limit, limit * (page - 1)]
+  );
 
-    return result;
-}
+  if (result.length === 0) {
+    return { error: "No posts found." };
+  }
+
+  return result;
+};
 
 const selectByTitle = async (title) => {
-    const [ result ] = await db.query('SELECT id FROM post WHERE title = ?', [ title ]);
-    return result;
-}
+  const [result] = await db.query("SELECT id FROM post WHERE title = ?", [
+    title,
+  ]);
+  return result;
+};
 
 const insert = async ({ title, description, category, author_id }) => {
-    
-    const createdAt = dayjs().format('YYYY-MM-DD');
+  const created_at = dayjs().format("YYYY-MM-DD");
 
-    const [ result ] = await db.query(`
+  const [result] = await db.query(
+    `
         INSERT INTO post 
-        (title, description, createdAt, category, author_id) 
+        (title, description, created_at, category, author_id) 
         VALUES (?, ?, ?, ?, ?)
-        `, [ title, description, createdAt, category, author_id ]);
-    
-        return {post: { title, description, createdAt, category, author_id }, result };
-}
+        `,
+    [title, description, created_at, category, author_id]
+  );
+
+  return {
+    post: { title, description, created_at, category, author_id },
+    result,
+  };
+};
 
 module.exports = { selectAll, selectByAuthorId, selectByTitle, insert };
